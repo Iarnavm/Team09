a07221b8edf103887249ef94711c4eb865617d7f
Anchal Tanwar <86921001+anchaltanwar@users.noreply.github.com>
2023-09-26 13:59:37 +0530

    Update GlobalNavigationManager.kt

--- SourceFile/src/main/java/ru/aleshin/timeplanner/navigation/GlobalNavigationManager.kt
+++ SourceFile/src/main/java/ru/aleshin/timeplanner/navigation/GlobalNavigationManager.kt
@@ -5,9 +5,7 @@ import ru.aleshin.core.utils.navigation.Router
 import ru.aleshin.timeplanner.presentation.ui.tabs.TabsScreen
 import javax.inject.Inject
 
-/**
- * @author Stanislav Aleshin on 14.02.2023.
- */
+
 interface GlobalNavigationManager {
 
     fun showTabScreen()
