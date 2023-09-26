3118a7fbc43bac2c80a136b56045947c2310f512
Anchal Tanwar <86921001+anchaltanwar@users.noreply.github.com>
2023-09-26 13:53:18 +0530

    Update Navigations.kt

--- SourceFile/src/main/java/ru/aleshin/timeplanner/di/annotation/Navigations.kt
+++ SourceFile/src/main/java/ru/aleshin/timeplanner/di/annotation/Navigations.kt
@@ -3,9 +3,7 @@ package ru.aleshin.timeplanner.di.annotation
 
 import javax.inject.Qualifier
 
-/**
- * @author Stanislav Aleshin on 18.02.2023.
- */
+
 @Qualifier
 annotation class GlobalNavigation
 
